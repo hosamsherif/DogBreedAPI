0578c43d87e71dc264b88b273ffe8ad9c7711bd6
hosamsherif <hosamsherif2000@gmail.com>
2021-05-28 01:43:44 +0200

    new commit

--- dog_breeds_API.py
+++ dog_breeds_API.py
@@ -47,4 +47,4 @@ async def predict_api():
     #return output
     return "hossam"
 if __name__ =="__main__":
-    uvicorn.run(app , port=8000 ,host='0.0.0.0')
+    uvicorn.run(app , port=8080 ,host='0.0.0.0')

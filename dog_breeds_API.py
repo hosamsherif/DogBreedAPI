c05a5bffc8ff51dc65d8074a340505c3c83718d1
hosamsherif <hosamsherif2000@gmail.com>
2021-05-28 14:57:17 +0200

    new commit

--- dog_breeds_API.py
+++ dog_breeds_API.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI,File,UploadFile
-##from tensorflow import keras
+from tensorflow import keras
 #import tensorflow as tf
 ##import uvicorn
-##import cv2
-##import numpy as np
+import cv2
+import numpy as np
 #import  base64
 #from PIL import Image
 import io
 app=FastAPI()
-'''    
+
 Filtered_breeds=['beagle', 'chihuahua', 'doberman','french_bulldog', 'golden_retriever', 'malamute', 'pug',
                  'saint_bernard', 'scottish_deerhound','tibetan_mastiff']
 
@@ -32,19 +32,19 @@ def predict(image,model):
         'breed ':Filtered_breeds[breed_index],
         'score ':str(pred[0][breed_index])
         }
-'''
+
 
 @app.get('/index')
 async def hello_world():
     return "Hello world!"
 
 @app.post("/predict/image")
-async def predict_api():
-    #async def predict_api(file: UploadFile = File(...)):
-    #image=read_image(await file.read())
-    #model=load_model()
-    #output=predict(image,model)
-    #return output
-    return "hossam"
+async def predict_api(file: UploadFile = File(...)):
+    image=read_image(await file.read())
+    model=load_model()
+    output=predict(image,model)
+    return output
+    #async def predict_api():
+    #return "hossam"
 #if __name__ =="__main__":
  #   uvicorn.run(app , port=8000 ,host='0.0.0.0')
